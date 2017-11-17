9030a85add316fb66309ce19946ea8deb0cc63f0
kvladimirovich10 <kvladimirovich10@gmail.com>
2017-11-17 03:02:16 +0300

    Initial commit task 1

---
+++ hometask6/src/main/java/hometask6/task2/PropertiesFileNotFoundException.java
@@ -0,0 +1,7 @@
+package hometask6.task2;
+
+public class PropertiesFileNotFoundException extends Throwable {
+    public PropertiesFileNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ hometask6/src/main/java/hometask6/task2/PropertyKeyNotFoundException.java
@@ -0,0 +1,7 @@
+package hometask6.task2;
+
+public class PropertyKeyNotFoundException extends Exception {
+    public PropertyKeyNotFoundException(String message) {
+        super(message);
+    }
+}
